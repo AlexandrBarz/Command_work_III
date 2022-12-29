4ef7a9ae8ee7757f599379fb452595b82570e82d
Terekhin <blackrat72@qip.ru>
2022-12-29 23:08:44 +0300

    add find + слияние

--- find.py
+++ find.py
@@ -1,8 +1,8 @@
 from functions import read_from_csv, list_to_string
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 
 
-def find(update: Update, _):
+def find_contact(update: Update, _):
     name = update.message.text.split()        
     if len(name) == 1:
         name.append(" ")
@@ -12,6 +12,9 @@ def find(update: Update, _):
         if name[0].capitalize() in char[0] or name[1].capitalize() in char[1] or name[1].capitalize() in char[0] or name[0].capitalize() in char[1]:
             find.append(char)                        
             update.message.reply_text(f"Найден контакт:\n {list_to_string(find)}")
+            update.message.reply_text('Выбери, что ты хочешь сделать.'
+            'Команда /cancel, чтобы завершить.\n',
+            reply_markup=ReplyKeyboardMarkup([['Add contact', 'Find contact', 'Change contact', 'Delete contact']], one_time_keyboard=True),)
             return 0
     else:
         update.message.reply_text(f"Ничего не удалось найти. Попобуете еще раз\n"
